149c0922cb594e36abfd994f4617f1939df23b16
iconicmore0 <iconicmore0@gmail.com>
2025-05-19 10:02:37 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,27 @@
+
+const airdrops = [
+  {
+    name: "0G Labs",
+    description: "Tesnet storage dan AI, tugas upload file & swap token",
+    status: "Ongoing",
+    link: "https://0g.ai/"
+  },
+  {
+    name: "Somnia",
+    description: "Tesnet dengan elemen Social-Fi dan interaksi digital ID",
+    status: "Ongoing",
+    link: "https://somnia.io/"
+  },
+  {
+    name: "KITE AI",
+    description: "Tesnet AI berbasis interaksi harian dengan karakter AI",
+    status: "Ongoing",
+    link: "https://kiteai.xyz/"
+  },
+  {
+    name: "Black Mirror",
+    description: "Tesnet dengan elemen misteri, AI & interaksi sosial",
+    status: "Ongoing",
+    link: "https://blackmirror.xyz/"
+  }
+];
